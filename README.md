ee6f9517c115a1e00d977812c32013d09f80342c
Armaan Lala <armaanlala@gmail.com>
2022-12-15 21:04:59 -0500

    Better Arduino Sketch

--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 This is my project for The Retrofuturistic Hardware VIP Fall '22
 
 ## Progress
-
 - Set up KiCAD Project + Github Repo
 - Created schematic for HEF4501BP Chip
 - Created schematic for entire keyboard PCB
 - Simple Arduino Sketch to read analog values from the keyboard
+
+
+
+## Main Issue
+- Unable to figure out purpose of two buttons
+- Unable to trace buttons and their output
 
 ![Schematic.png](Schematic.png)
--- read_analog/read_analog.ino
+++ read_analog/read_analog.ino
@@ -1,5 +1,5 @@
-#define RED 5V
-#define BLACK GND
+#define RED GND
+#define BLACK 5V
 #define BROWN A0
 #define ORANGE 37
 #define YELLOW 41
@@ -8,31 +8,94 @@
 #define PURPLE 53
 
 int val = 0;
+int sliders[8];
+int knobs[8];
 
 void setup() {
+    pinMode(ORANGE, OUTPUT);
+    pinMode(YELLOW, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    pinMode(PURPLE, OUTPUT);
+    pinMode(BROWN, INPUT);
 
-  pinMode(ORANGE, OUTPUT);
-  pinMode(YELLOW, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  pinMode(PURPLE, OUTPUT);
-  pinMode(BROWN, INPUT);
+    digitalWrite(ORANGE, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(GREEN, LOW);
 
-  digitalWrite(ORANGE,LOW);
-  digitalWrite(YELLOW,LOW);
-  digitalWrite(GREEN,LOW);
-  digitalWrite(PURPLE,HIGH);
-  digitalWrite(BLUE,LOW);
+    digitalWrite(PURPLE, HIGH);
+    digitalWrite(BLUE, LOW);
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  // put your setup code here, to run once:
-
+    // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    val = analogRead(BROWN);
-	  Serial.println(val);
-    delay(1000);
+    // put your main code here, to run repeatedly:
+    digitalWrite(BLUE, LOW);
+    digitalWrite(PURPLE, HIGH);
+    // Slider
+    for (int i = 0; i < 8; i++) {
+        if (i % 2 > 0) {
+            digitalWrite(ORANGE, HIGH);
+        } else {
+            digitalWrite(ORANGE, LOW);
+        }
+        if (i % 4 > 1) {
+            digitalWrite(YELLOW, HIGH);
+        } else {
+            digitalWrite(YELLOW, LOW);
+        }
+        if (i % 8 > 3) {
+            digitalWrite(GREEN, HIGH);
+        } else {
+            digitalWrite(GREEN, LOW);
+        }
+        val = analogRead(BROWN);
+        if (abs(knobs[i] - val) > 5) {
+            knobs[i] = val;
+            Serial.print("Slider ");
+            Serial.print(i);
+            Serial.print(" changed to ");
+            Serial.println(val);
+        }
+        // delay(10);
+    }
+    // delay(10);
+    digitalWrite(PURPLE, LOW);
+    digitalWrite(BLUE, HIGH);
+
+    // Knobs
+    for (int i = 0; i < 8; i++) {
+        if (i % 2 > 0) {
+            digitalWrite(ORANGE, HIGH);
+        } else {
+            digitalWrite(ORANGE, LOW);
+        }
+        if (i % 4 > 1) {
+            digitalWrite(YELLOW, HIGH);
+        } else {
+            digitalWrite(YELLOW, LOW);
+        }
+        if (i % 8 > 3) {
+            digitalWrite(GREEN, HIGH);
+        } else {
+            digitalWrite(GREEN, LOW);
+        }
+        val = analogRead(BROWN);
+        val = analogRead(BROWN);
+        if (abs(sliders[i] - val) > 5) {
+            sliders[i] = val;
+            Serial.print("Rotary Knob ");
+            Serial.print(i);
+            Serial.print(" changed to ");
+            Serial.println(val);
+        }
+        // delay(10);
+    }
+
+    // val = analogRead(BROWN);
+    // Serial.println(val);
+    // delay(100);
 }
